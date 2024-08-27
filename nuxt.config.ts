669fcac3f18e194452979cba42b200bfacf95cde
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-08-27 00:29:45 -0300

    config vuetify... again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  imports:{
+    dirs:['store']
+  },
+  css:[
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesingicons.min.css'
+  ],
+
+  build:{
+    transpile: ['vuetify']
+  }
 })

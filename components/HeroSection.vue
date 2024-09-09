f9be8821b19582b0a5129151abc2c49199321794
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-09-09 15:27:24 -0300

    changes into hero-section component done for now

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -5,8 +5,8 @@
     <v-row align="center" justify="center">
       <v-col cols="12" md="8" class="text-center">
           <h1 class="hero-title text-h1">{{ title }}</h1>
-          <p class="subtitle-1 text-h4">{{ subtitle }}</p>
-          <v-btn large color="primary" @click="ctaAction">{{ ctaText }}</v-btn>
+          <p class="hero-subtitle text-h4">{{ subtitle }}</p>
+          <v-btn default outlined class="custom-button" @click="ctaAction">{{ ctaText }}</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -26,7 +26,7 @@ export default {
     },
     ctaText: {
       type: String,
-      default: 'Conoce más'
+      default: 'Contactanos'
     }
   },
   methods: {
@@ -56,9 +56,26 @@ export default {
 }
 
 .hero-title {
-    font-family: "Bebas Neue", cursive;
-    font-weight: bolder;
-    font-style: italic;
-    color: yellow;
+  font-family: "Bebas Neue", cursive;
+  font-weight: bolder;
+  font-style: italic;
+  color: yellow;
 }
+
+.hero-subtitle {
+  font-family: "Bebas Neue", cursive;
+  font-weight: bolder;
+  font-style: italic;
+  color: white;
+}
+
+.custom-button {
+  transition: background-color 0.3s, color 0.3s; /* Añade una transición suave */
+}
+
+.custom-button:hover {
+  background-color: #ff5722; /* Cambia el color de fondo al pasar el cursor */
+  color: #ffffff; /* Cambia el color del texto al pasar el cursor */
+}
+
 </style>

ca14ae84307318ba5a55812f26da22e3d1deb507
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-09-16 15:36:53 -0300

    problem of style of heroContent resolved

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,18 +1,20 @@
-
-
 <template>
-  <v-container fluid class="hero-section" fill-height>
+  <v-container :style="heroStyle" fluid fill-height>
     <v-row align="center" justify="center">
-      <v-col cols="12" md="8" class="text-center">
-          <h1 class="hero-title text-h1">{{ title }}</h1>
-          <p class="hero-subtitle text-h4">{{ subtitle }}</p>
-          <v-btn default outlined class="custom-button" @click="ctaAction">{{ ctaText }}</v-btn>
+      <v-col cols="12" md="8" class="text-center" :style="contentStyle">
+        <h1 class="text-h1" :style="titleStyle">{{ title }}</h1>
+        <p class="text-h4" :style="subtitleStyle">{{ subtitle }}</p>
+        <v-btn outlined :style="buttonStyle" @mouseover="hoverButton" @mouseleave="resetButton" @click="ctaAction">
+          {{ ctaText }}
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import portadaImage from '@/assets/portada.index.jpg';
+
 export default {
   name: 'HeroSection',
   props: {
@@ -29,53 +31,71 @@ export default {
       default: 'Contactanos'
     }
   },
+  data() {
+    return {
+      buttonStyle: {
+        transition: 'background-color 0.3s, color 0.3s',
+        backgroundColor: 'black',
+        color: 'white',
+        borderColor: 'black' // También ajusta el color del borde
+      }
+    };
+  },
+  computed: {
+    heroStyle() {
+      return {
+        backgroundImage: `url(${portadaImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative'
+      };
+    },
+    contentStyle() {
+      return {
+        padding: '20px',
+        borderRadius: '10px'
+      };
+    },
+    titleStyle() {
+      return {
+        fontFamily: '"Bebas Neue", cursive',
+        fontWeight: 'bolder',
+        fontStyle: 'italic',
+        color: 'yellow'
+      };
+    },
+    subtitleStyle() {
+      return {
+        fontFamily: '"Bebas Neue", cursive',
+        fontWeight: 'bolder',
+        fontStyle: 'italic',
+        color: 'white'
+      };
+    }
+  },
   methods: {
     ctaAction() {
       this.$router.push('/contacto');
+    },
+    hoverButton() {
+      this.buttonStyle = {
+        ...this.buttonStyle,
+        backgroundColor: '#ff5722',
+        color: '#ffffff',
+        borderColor: '#ff5722'
+      };
+    },
+    resetButton() {
+      this.buttonStyle = {
+        backgroundColor: 'black',
+        color: 'white',
+        borderColor: 'black'
+      };
     }
   }
 };
 </script>
-
-<style scoped>
-.hero-section {
-  background-image: url('../assets/portada.index.jpg');
-  background-size: cover;
-  background-position: center;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-}
-
-.hero-content {
-  background-color: rgba(0, 0, 0, 0.5); /* Fondo oscuro semitransparente para mayor legibilidad */
-  padding: 20px;
-  border-radius: 10px;
-}
-
-.hero-title {
-  font-family: "Bebas Neue", cursive;
-  font-weight: bolder;
-  font-style: italic;
-  color: yellow;
-}
-
-.hero-subtitle {
-  font-family: "Bebas Neue", cursive;
-  font-weight: bolder;
-  font-style: italic;
-  color: white;
-}
-
-.custom-button {
-  transition: background-color 0.3s, color 0.3s; /* Añade una transición suave */
-}
-
-.custom-button:hover {
-  background-color: #ff5722; /* Cambia el color de fondo al pasar el cursor */
-  color: #ffffff; /* Cambia el color del texto al pasar el cursor */
-}
-
-</style>

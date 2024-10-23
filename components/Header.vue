199a329ed7e4f26932d1e1edc3d18f4fe29e5166
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-10-23 16:11:52 -0300

    changes into nav

--- components/Header.vue
+++ components/Header.vue
@@ -1,31 +1,45 @@
 <template>
     <nav>
-        <v-toolbar color="black">
-            <v-btn to="/" flat color="white">
-                <span class="custom-text">Logo</span>
+        <v-toolbar color="black" height="80"> <!-- Ajusta la altura del toolbar si es necesario -->
+            <!-- Botón de navegación sin estilo -->
+            <v-btn class="nav-button" to="/" overlay="false">
+                <img src="../assets/title_nav.png" class="logo-image">
             </v-btn>
-            <v-toolbar-title>
-                <router-link to="/" class="custom-text">Matafuegos Noble</router-link>
-            </v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn to="/" flat color="white">
+            <v-btn to="/" variant="text">
                 <span class="custom-text">Inicio</span>
             </v-btn>
-            <v-btn to="/contacto" flat color="white">
+            <v-btn to="/contacto" variant="text">
                 <span class="custom-text">Contacto</span>
             </v-btn>
         </v-toolbar>
     </nav>
 </template>
 
-
 <style scoped>
-
 .custom-text {
-    font-family: 'Bebas Neue';
+    font-family: 'Bebas Neue', cursive;
+    font-weight: bolder;
+    font-style: italic;
     color: yellow;
-    font-size: 150%; 
+    font-size: 150%;
 }
 
-</style>
+/* Ajusta el tamaño de la imagen del logo */
+.logo-image {
+    max-width: 300px; /* Ajusta el tamaño de la imagen del logo */
+    height: auto;
+}
 
+/* Ajusta el botón para centrarlo */
+.nav-button {
+    margin: 0 auto; /* Centra el botón horizontalmente */
+    display: flex; /* Usa flex para centrar verticalmente */
+    align-items: center; /* Alinea verticalmente el contenido del botón */
+}
+
+/* Para ocultar el overlay del botón (opcional) */
+.nav-button:deep(.v-btn__overlay) {
+    opacity: 0;
+}
+</style>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,18 +1,20 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
+
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
 export default defineNuxtPlugin((nuxtApp) => {
-const vuetify = createVuetify({
-    components,
-    directives,
-    ssr: true,
-    theme: {
-        defaultTheme: 'dark',
-    },
-});
+    
+    const vuetify = createVuetify({
+        components,
+        directives,
+        ssr: true,
+        theme: {
+            defaultTheme: 'dark',
+        },
+    });
 
-nuxtApp.vueApp.use(vuetify);
+    nuxtApp.vueApp.use(vuetify);
 });

fa786656e358a89db587d83efe551921e0b9d98d
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-10-29 14:31:24 -0300

    fixing nav part 3..

--- components/Header.vue
+++ components/Header.vue
@@ -1,46 +1,47 @@
 <template>
-        <!-- Drawer para navegación en pantallas pequeñas -->
-        <v-navigation-drawer
-            v-model="drawer"
-            temporary
-            left
-        >
-            <!-- Drawer para pantallas pequeñas -->
-            <v-list>
-                <v-list-item link to="/">
-                    <v-list-item-content>
-                        <v-list-item-title>Inicio</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link to="/contacto">
-                    <v-list-item-content>
-                        <v-list-item-title>Contacto</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+    <!-- Drawer para navegación en pantallas pequeñas -->
+    <v-navigation-drawer
+        v-model="drawer"
+        temporary
+        left
+        @click:close="drawer = false"
+    >
+        <!-- Drawer para pantallas pequeñas -->
+        <v-list>
+            <v-list-item to="/" @click.stop>
+                <v-list-item-content>
+                    <v-list-item-title>Inicio</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item to="/contacto" @click.stop>
+                <v-list-item-content>
+                    <v-list-item-title>Contacto</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
 
-        <!-- Barra de navegación -->
-        <v-app-bar color="black" height="80">
-            <!-- Icono de menú para abrir el drawer en pantallas pequeñas -->
-            <v-app-bar-nav-icon @click="drawer = !drawer" class="d-md-none" color="yellow" />
+    <!-- Barra de navegación -->
+    <v-app-bar color="black" height="80">
+        <!-- Icono de menú para abrir el drawer en pantallas pequeñas -->
+        <v-app-bar-nav-icon @click="drawer = !drawer" class="d-md-none" color="yellow" />
+        
+        <!-- Botón de navegación sin overlay -->
+        <v-btn class="nav-button" to="/" :overlay="false">
+            <img src="../assets/title_nav.png" class="logo-image" alt="Logo" />
+        </v-btn>
 
-            <!-- Botón de navegación sin overlay -->
-            <v-btn class="nav-button" href="/" :overlay="false">
-                <img src="../assets/title_nav.png" class="logo-image" alt="Logo" />
+        <!-- Alinea los botones al extremo derecho -->
+        <v-spacer></v-spacer>
+        <div class="d-none d-md-flex">
+            <v-btn class="nav-button" to="/" variant="text" :overlay="false" @click.stop>
+                <span class="custom-text">Inicio</span>
             </v-btn>
-
-            <!-- Alinea los botones al extremo derecho -->
-            <v-spacer></v-spacer>
-            <div class="d-none d-md-flex" >
-                <v-btn class="nav-button" href="/" variant="text" :overlay="false">
-                    <span class="custom-text">Inicio</span>
-                </v-btn>
-                <v-btn class="nav-button" href="/contacto" variant="text" :overlay="false">
-                    <span class="custom-text">Contacto</span>
-                </v-btn>
-            </div>
-        </v-app-bar>
+            <v-btn class="nav-button" to="/contacto" variant="text" :overlay="false" @click.stop>
+                <span class="custom-text">Contacto</span>
+            </v-btn>
+        </div>
+    </v-app-bar>
 </template>
 
 <script>
@@ -62,7 +63,6 @@ export default {
     font-size: 150%;
 }
 
-
 .logo-image {
     max-width: 300px; 
     height: auto;
@@ -72,7 +72,6 @@ export default {
     display: flex;
     align-items: center;
 }
-
 
 .v-app-bar-nav-icon {
     margin-right: 16px;

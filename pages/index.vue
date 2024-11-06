94494a12b530be80fc5cad8b3f511afceb821522
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-11-06 09:19:27 -0300

    testing with AboutUs component

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,18 @@
 <template>
     <div>
     <HeroSection />
-    <AboutUs />
     <ServicesCard/>
     </div>
 </template>
 
 <script>
 import HeroSection from '~/components/HeroSection.vue';
-import AboutUs from '~/components/AboutUs.vue';
 import ServicesCard from '~/components/ServicesCard.vue';
 
 
 export default {
     components: {
         HeroSection,
-        AboutUs,
         ServicesCard
     }
 }
